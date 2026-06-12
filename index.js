ff5cb2c3ee1e7c2375db615d11dc24ed2cf6edbc
•PɪRᴀTᴇ• <78387329+BadreddineOtmane@users.noreply.github.com>
2026-06-12 11:59:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const port = 3000
 
-app.get('/', (req, res) => res.send('Pirate is the best dev ever  JavaScipt god!'))
+app.get('/', (req, res) => res.send('Pirate is the best dev ever!'))
 
 app.listen( port , () =>
 console.log(`Your app is playing a .help By PiRaTe`)
